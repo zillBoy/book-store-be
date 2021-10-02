d0859dda78cf2c2df5b82a54222b21ab2a8603c3
Muhammad Usman <prousman741@gmail.com>
2021-10-02 14:56:21 +0500

    Express static - able to view the images

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const booksRouter = require('./routes/books/books.router')
 
 const app = express()
@@ -9,5 +10,6 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }))
 app.use('/books', booksRouter)
+app.use(express.static(path.join(__dirname, 'public')))
 
 module.exports = app
