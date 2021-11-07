c1d4f30be2cb9cf8ac72ca73c16fe930c14f8ac7
Muhammad Usman <prousman741@gmail.com>
2021-11-07 12:52:59 +0500

    make it better

--- src/app.js
+++ src/app.js
@@ -6,9 +6,8 @@ const booksRouter = require('./routes/books/books.router')
 const app = express()
 
 app.use(express.json())
-app.use(cors({
-    origin: 'http://localhost:3000'
-}))
+app.use(cors()) // { origin: 'http://localhost:3000' }
+
 app.use('/books', booksRouter)
 app.use(express.static(path.join(__dirname, 'public')))
 
